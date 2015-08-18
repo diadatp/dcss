13829e39a679c1a437497446abaefd492cfe9f30
diadatp <admin@diadatp.com>
2015-08-18 06:08:32 +0530

    Added TTH resolution. Fixed indentation. Working on TTH queue.

--- dcss.js
+++ dcss.js
@@ -1,7 +1,7 @@
-if (!Array.prototype.last){
-    Array.prototype.last = function(){
-        return this[this.length - 1];
-    };
+if (!Array.prototype.last) {
+	Array.prototype.last = function() {
+		return this[this.length - 1];
+	};
 };
 
 if (!('contains' in String.prototype)) {
@@ -13,9 +13,15 @@ if (!('contains' in String.prototype)) {
 var net = require('net');
 var HashMap = require('hashmap');
 var lowdb = require('lowdb')
+var queue = require('queue');
+
+var tthQueryQueue = queue({
+	'concurrency': 1
+});
 
 var db = lowdb('db.json')
-var nickIPmap = new HashMap();
+var nickIpMap = new HashMap();
+var tthFilenameMap = new HashMap();
 
 var first = 0;
 var hub_ip = '10.3.14.10';
@@ -25,49 +31,76 @@ var search_timeout = 5000;
 // var hub_ip = 'brianzaland.ex3menet.com';
 // var hub_port = 443;
 
-var client = net.connect({host: hub_ip, port: hub_port}, function() {
+var client = net.connect({
+	host: hub_ip,
+	port: hub_port
+}, function() {
 	console.log('Connected to server.');
 	setTimeout(function() {
 		client.write('$Supports UserCommand UserIP2|$Key 011010110110010101111001|$ValidateNick johndoe|$Version 1,0091|$GetNickList|$MyINFO $ALL johndoe <++ V:0.673,M:A,H:1/0/0,S:3>$ $LAN(T3)0x31$example@example.com$26843545600$|');
-//		first = 1;
+		// first = 1;
 	}, 1000);
 
-	setInterval(function() {
-		console.log('searched');
-		// client.write('$Search Hub:johndoe F?T?0?1?sex|');
-	}, 5000);
+	// setInterval(function() {
+	// 	console.log('searched');
+	// 	client.write('$Search Hub:johndoe F?T?0?1?sex|');
+	// }, search_timeout);
 });
 
 client.on('data', function(data) {
 	data = data.toString().split('|');
 	// console.log(data);
 	data.forEach(function(element, index, array) {
+		// console.log('e ' + element);
 		var tokens = element.split(' ');
-		switch(tokens[0]) {
+		switch (tokens[0]) {
 			case '$Search':
-				db('searches').push({'nick': tokens[1], hub: 'tit'});
-				// console.log('search ' + tokens);
+				// console.log('s ' + tokens);
+				var time = new Date().getTime();
+				if (tokens[1].contains('Hub:')) {
+					var nick = tokens[1].slice(4);
+					var ip = '';
+				} else {
+					var nick = '';
+					var ip = tokens[1].split(':')[0];
+				}
+				var isTTH = ('9' == tokens[2].charAt(6));
+				if (isTTH) {
+					var term = tokens[2].split('TTH:').last();
+					if (tthFilenameMap.has(term)) {
+						tthQueryQueue.push(function(cb) {
+							setTimeout(function() {
+								console.log('$ added tth to queue');
+								client.write('$Search Hub:johndoe F?T?0?9?TTH:' + term + '|');
+								cb();
+							}, search_timeout);
+						});
+						tthQueryQueue.start();
+					}
+				} else {
+					var term = tokens[2].split('?').last().split('$').join(' ');
+				}
+				console.log(time + ' ' + nick + ' ' + ip + ' ' + isTTH + ' ' + term);
+				// db('searches').push({'nick': tokens[1], hub: 'tit'});
 				// client.write("$Search Hub:johndoe F?T?0?9?TTH:3WPUB5PVZ5IW3UGUGUKASJHBY5FKCSLKZGVAXBQ|");
 				// client.write("$Search Hub:johndoe F?T?0?1?sex|");
-				if(tokens[1].contains('Hub:')) {
-					deferredLogNick(new Date().getTime(), tokens);
-				} else {
-					deferredLogIP(new Date().getTime(), tokens);
-				}
 				break;
 			case '$MyINFO':
-				// console.log('nick: ' + nick);
+				// console.log('i ' + tokens);
 				var nick = element.split(' ')[2];
 				client.write('$ConnectToMe ' + nick + ' 10.4.1.63:55555|');
 				break;
 			case '$SR':
-				if((tokens.length < 5) || (!tokens.last().contains('(' + hub_ip + ':' + hub_port + ')'))) {
+				console.log('sr ' + tokens);
+				if ((tokens.length < 5) || (!tokens.last().contains('(' + hub_ip + ':' + hub_port + ')'))) {
 					break;
 				}
-				// console.log('sr ' + tokens);
 				var filename = tokens.slice(2, -2).join(' ').split('\\').last();
 				var tth = tokens.slice(1, -1).last().split('TTH:').last();
-				// db('tthmap').push({'tth': tth, 'filename': filename});
+				db('tthmap').push({
+					'tth': tth,
+					'filename': filename
+				});
 				// console.log('sr ' + filename + ' ' + tth);
 				break;
 			default:
@@ -76,21 +109,21 @@ client.on('data', function(data) {
 		}
 	});
 
-	if(1 == first) {
+	if (1 == first) {
 		client.write("<johndoe> dsdsds|");
 	}
 });
 
 function deferredLogNick(time, x) {
-//	console.log(x[0].slice(4) + x[1]);
+	// console.log(x[0].slice(4) + x[1]);
 	client.write('$ConnectToMe ' + x[1].slice(4) + ' 10.4.1.63:55555|');
-	if('9' == x[2].charAt(6)) {
-//		console.log('tth');
+	if ('9' == x[2].charAt(6)) {
+		// console.log('tth');
 	}
 }
 
 function deferredLogIP(time, x) {
-//	console.log(x[0] + x[1]);
+	// console.log(x[0] + x[1]);
 }
 
 function resolveTTH(tth) {
@@ -101,18 +134,18 @@ var server = net.createServer(function(c) {
 	c.on('data', function(data) {
 		var nick = data.toString().split('|')[0].split(' ')[1];
 		var ip = c.remoteAddress.split(':').last();
-		if(nickIPmap.has(nick)){
-			if(nickIPmap.get(nick) != ip) {
+		if (nickIpMap.has(nick)) {
+			if (nickIpMap.get(nick) != ip) {
 				console.log("someone changed ip?");
 			}
 		}
-		console.log(nick + ' - ' + ip + ' ' + resolveHostel(ip));
+		// console.log(nick + ' - ' + ip + ' ' + resolveHostel(ip));
 		// db('nicks').push({'nick': nick, 'ip': ip});
-		nickIPmap.set(nick, ip);
+		nickIpMap.set(nick, ip);
 		c.end();
 	});
 	c.on('error', function(e) {
-        	console.log('problem with request: ' + e.message);
+		console.log('NRSC roblem with request: ' + e.message);
 	});
 
 });
@@ -132,16 +165,16 @@ client.on('end', function() {
 
 function resolveHostel(ip) {
 	ip = ip.split('.');
-	if(4 != ip.length) {
+	if (4 != ip.length) {
 		return 'err';
 	}
-	switch(ip[1]) {
+	switch (ip[1]) {
 		case '1':
 			return 'CC';
 		case '2':
 			return 'LABS';
 		case '3':
-			switch(ip[2]) {
+			switch (ip[2]) {
 				case '1':
 					return 'AH1';
 				case '2':
@@ -166,7 +199,7 @@ function resolveHostel(ip) {
 					return 'err';
 			}
 		case '4':
-			switch(ip[2]) {
+			switch (ip[2]) {
 				case '1':
 					return 'CH1';
 				case '2':
